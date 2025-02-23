88f27d21a18810e476d4085fc3ffaaba7db090fc
Ari-manius <147991895+Ari-manius@users.noreply.github.com>
2025-02-23 21:14:26 +0100

    readded the other apps, Mbilization Checked

--- Seminar-Survey/settings.py
+++ Seminar-Survey/settings.py
@@ -18,7 +18,7 @@ SESSION_CONFIGS = [
         name='seminar_survey',
         display_name='Seminar Survey',
         num_demo_participants=10,
-        app_sequence=['StartApp','EndApp'],
+        app_sequence=["MobilizationApp"],
     ),
 ]
 
